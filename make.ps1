a2189092a09514c0d4b6ac4dc10f84a75a71ccd7
wangyao <1822364545@qq.com>
2024-07-08 22:34:50 +0800

    modify path

--- make.ps1
+++ make.ps1
@@ -5,10 +5,10 @@ echo "`nRemove-Item -Path ./build -Recurse -Force"
 Remove-Item -Path ./build -Recurse -Force
 
 # user / system env
-# $env:QT_QPA_PLATFORM_PLUGIN_PATH = "D:\Library\5.15.2\msvc2019_64\plugins\platforms"
+# $env:QT_QPA_PLATFORM_PLUGIN_PATH = "D:\Library\Qt\5.15.2\msvc2019_64\plugins\platforms"
 
-echo "`nsource: cmake -S . -B build -G Ninja -D qt5rootdir=D:/Library/5.15.2/msvc2019_64"
-cmake -S . -B build -G Ninja -D qt5rootdir=D:/Library/5.15.2/msvc2019_64
+echo "`nsource: cmake -S . -B build -G Ninja -D qt5rootdir=D:/Library/Qt/5.15.2/msvc2019_64"
+cmake -S . -B build -G Ninja -D qt5rootdir=D:/Library/Qt/5.15.2/msvc2019_64
 
 echo "`nbuild: cmake --build build -j"
 cmake --build build -j
